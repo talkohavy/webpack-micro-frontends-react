dda89f391dbad8fddeb1ffd5195635cda2c82c67
Tal Kohavy <talkohavy@gmail.com>
2024-01-30 01:39:54 +0200

    added a start (dev) script using the dev server

--- apps/app-01/webpack.config.js
+++ apps/app-01/webpack.config.js
@@ -2,6 +2,13 @@
 const webpackConfig = {
   mode: 'development',
   entry: './src/index.js',
+
+  // ---------------------
+  // Section 2: dev server
+  // ---------------------
+  devServer: {
+    port: 8001,
+  },
 };
 
 export default webpackConfig;
