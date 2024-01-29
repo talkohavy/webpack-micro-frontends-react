c0f83b8972664618b1e124ed4728ce60ef3fc4ad
Tal Kohavy <talkohavy@gmail.com>
2024-01-30 01:50:35 +0200

    added an public/index.html & added the HTML plugin to the webpack config

--- apps/app-01/webpack.config.js
+++ apps/app-01/webpack.config.js
@@ -1,5 +1,10 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 /** @type {import('webpack').Configuration} */
 const webpackConfig = {
+  // ---------------------------
+  // Section 1: Basic properties
+  // ---------------------------
   mode: 'development',
   entry: './src/index.js',
 
@@ -9,6 +14,15 @@ const webpackConfig = {
   devServer: {
     port: 8001,
   },
+
+  // ------------------
+  // Section 3: plugins
+  // ------------------
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
 
 export default webpackConfig;
