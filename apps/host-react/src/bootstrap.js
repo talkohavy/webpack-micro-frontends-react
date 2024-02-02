45894a8651956dc654277686e7c4b87a3c6ed3ed
Tal Kohavy <talkohavy@gmail.com>
2024-02-02 19:23:34 +0200

    react-host main.js file updated to new react 18

--- apps/host-react/src/bootstrap.js
+++ apps/host-react/src/bootstrap.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.js';
 
-const el = document.getElementById('root');
+function Client() {
+  return <App />;
+}
 
-ReactDOM.render(<App />, el);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Client />);
