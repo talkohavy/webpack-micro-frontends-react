2fcdd828f097c1ff82902b774d41bcc413f968ec
Tal Kohavy <talkohavy@gmail.com>
2024-02-02 08:30:57 +0200

    created a react host

---
+++ apps/host-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div
+      style={{
+        border: '1px solid #f15b2a',
+        backgroundColor: '#fcd4c7',
+        textAlign: 'center',
+        padding: '20px 0',
+        color: '#f15b2a',
+      }}
+    >
+      Container: React
+    </div>
+  );
+}
