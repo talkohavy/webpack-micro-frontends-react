5883ac2aa08af695ee49a00d6afc38003a65b2df
Tal Kohavy <talkohavy@gmail.com>
2024-03-22 12:56:54 +0200

    save isLogged in localStorage

--- apps/host/src/bootstrap.jsx
+++ apps/host/src/bootstrap.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import { Provider as StoreProvider } from 'react-redux';
 import { HistoryRouter } from 'redux-first-history/rr6';
 import App from './App';
 import GlobalErrorBoundaryDevelopment from './components/ErrorBoundaries/GlobalErrorBoundaryDevelopment';
@@ -8,19 +8,26 @@ import DarkThemeProvider from './providers/DarkThemeProvider';
 import { configureMyStore } from './store/index';
 import './index.css';
 
-const { store, history } = configureMyStore({});
+/** @typedef {import('./store/types').State} State */
+
+const isLogged = !!localStorage.getItem('isLogged');
+
+/** @type {Partial<State>} */
+const preloadedState = { user: { isLogged } };
+
+const { store, history } = configureMyStore({ preloadedState });
 
 function Client() {
   return (
     <React.StrictMode>
       <GlobalErrorBoundaryDevelopment>
-        <Provider store={store}>
+        <StoreProvider store={store}>
           <HistoryRouter history={history}>
             <DarkThemeProvider>
               <App />
             </DarkThemeProvider>
           </HistoryRouter>
-        </Provider>
+        </StoreProvider>
       </GlobalErrorBoundaryDevelopment>
     </React.StrictMode>
   );
